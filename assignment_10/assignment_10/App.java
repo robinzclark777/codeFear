825d97d424b7cf350867d3a6d0d471f2f2607fc3
Robin Clark <robinzclark@gmail.com>
2019-03-21 10:14:22 -0700

    assign 10

--- assignment_10/assignment_10/App.java
+++ assignment_10/assignment_10/App.java
@@ -20,8 +20,10 @@ public class App {
 	 */
 	
 	public static int partialSum(int a, int b, int c) {
-		
-		return 0;
+		if (a == 13) return 0;
+		if (b == 13) return a;
+		if (c == 13) return a + b;
+		return a + b + c;
 		
 	}
 
