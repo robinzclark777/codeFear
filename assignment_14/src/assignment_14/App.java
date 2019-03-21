8be323b2ae4d0591c66563c2481e9671cbd12147
Robin Clark <robinzclark@gmail.com>
2019-03-21 12:06:37 -0700

    assign 14 and 15

--- assignment_14/src/assignment_14/App.java
+++ assignment_14/src/assignment_14/App.java
@@ -21,7 +21,11 @@ public class App {
 	 */
 	
 	public static int matchingChar(String[] a, String[] b) {
-		return 0;
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].length() > 0 && b[i].length() > 0 && a[i].charAt(0) == b[i].charAt(0)) count++;
+		}
+		return count;
 	}
 
 	
--- assignment_15/src/assignment_15/App.java
+++ assignment_15/src/assignment_15/App.java
@@ -4,7 +4,6 @@ import ignore.TestingUtils;
 
 public class App {
 
-
 	/**
 	 * 
 	Given a string, return the length of the longest streak of the same chars in the string. 
@@ -19,23 +18,28 @@ public class App {
 	 */
 	
 	public static int longestStreak(String str) {
-		return 0;
+		int longest = 0;
+		int current = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (i == 0) {
+				current = 1;
+				longest = 1;
+				continue;
+			}
+			if (str.charAt(i) == str.charAt(i-1)) {
+				current++;
+				if (current > longest) longest = current;
+			} else {
+				current = 1;
+			}
 	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
-	public static void main(String args[]){
-		TestingUtils.runTests();
-		
-	}
+	return longest;
 }
 
+	// ----------------------STARTING POINT OF PROGRAM. IGNORE BELOW
+	// --------------------//
+	public static void main(String args[]) {
+		TestingUtils.runTests();
+
+	}
+}
