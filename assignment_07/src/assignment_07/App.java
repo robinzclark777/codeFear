28e19901b78dafd181ddff73008509147e426198
Robin Clark <robinzclark@gmail.com>
2019-03-21 09:52:06 -0700

    assignment 7

--- assignment_07/src/assignment_07/App.java
+++ assignment_07/src/assignment_07/App.java
@@ -21,7 +21,17 @@ public class App {
 		fizzyWizzy(3) <b>---></b> "Fizz!" <br>
 	 */
 	public static String fizzyWizzy(int n) {
-		    
+				boolean fizz = n % 3 == 0;
+				boolean buzz = n % 5 == 0;
+				if (fizz && buzz) {
+					return "FizzBuzz!";
+				}
+				if (fizz) {
+					return "Fizz!";
+				}
+				if (buzz) {
+					return "Buzz!";
+				}
 		    return n + "!";
 		    
 
